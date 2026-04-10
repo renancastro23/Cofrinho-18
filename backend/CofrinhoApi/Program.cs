28789bdd3add4c954894e7af0514beb41a7d8f06
Renan Castro <renancastro123306@gmail.com>
2026-04-10 18:10:31 -0300

    Resolvendo Build
    Alteração no Program.cs

--- backend/CofrinhoApi/Program.cs
+++ backend/CofrinhoApi/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+    options.AddPolicy(name: "AllowReact", policy =>
     {
         policy
             .WithOrigins("http://localhost:3000")
--- backend/CofrinhoApi/obj/Debug/net8.0/CofrinhoApi.AssemblyInfo.cs
+++ backend/CofrinhoApi/obj/Debug/net8.0/CofrinhoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CofrinhoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1ddb09df36bbe674f198acd3e0d9b95efa5843b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2cde617f2a3e030055677ca353fd90c1801934d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CofrinhoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CofrinhoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
